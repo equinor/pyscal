3272aa4643658f33fbeabccb9f44631977051845
Håvard Berland <havb@equinor.com>
2019-05-28 20:44:24 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="pyscal",
+    version="0.0.1",
+    description="Generate relperm include files for Eclipse 100",
+    url="http://github.com/equinor/pyscal",
+    author="Håvard Berland",
+    author_email="havb@equinor.com",
+    license="LGPLv3",
+    packages=["pyscal"],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+)
