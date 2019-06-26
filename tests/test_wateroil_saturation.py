3e2c3d1a2cb6e2675e1c77a4361be38ee988b391
Håvard Berland <havb@equinor.com>
2019-06-26 23:22:37 +0200

    increase deadline

--- tests/test_wateroil_saturation.py
+++ tests/test_wateroil_saturation.py
@@ -66,6 +66,7 @@ def test_wateroil_sorw(sorw):
 
 
 # Test combination of 2 floats as parameters:
+@settings(deadline=1000)
 @given(st.floats(min_value=0, max_value=1), st.floats(min_value=0, max_value=1))
 def test_wateroil_dual(p1, p2):
     try:
