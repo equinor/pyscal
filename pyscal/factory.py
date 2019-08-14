5849933c766879da8fa2c2cc315ad905e3cb90e6
Håvard Berland <havb@equinor.com>
2019-08-14 15:41:14 +0200

    Improve and maybe finish create_water_oil factory
    Capillary pressure is missing.
    
    Changed naming due to pylint, and also to static methods

--- pyscal/factory.py
+++ pyscal/factory.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from pyscal import WaterOil, GasOil, WaterOilGas
+from pyscal import WaterOil, GasOil, WaterOilGas, SCALrecommendation
 
 
 def slicedict(dct, keys):
@@ -14,75 +14,133 @@ def slicedict(dct, keys):
 
 # Sets of dictionary keys, presence of these
 # in incoming dictionary determines the codepaths. These must
-# again match the API of the functions downstream in e.g. WaterOil
-wo_endpoints = ["swirr", "swl", "swcr", "sorw"]
-wo_corey_water = ["nw"]
-wo_water_endpoints = ["krwmax", "krwend"]
-wo_corey_oil = ["now"]
-wo_let_water = ["Lw", "Ew", "Tw"]
-wo_let_oil = ["Low", "Eow", "Tow"]
-wo_oil_endpoints = ["kromax", "kroend"]
+# again match the API of the functions downstream in e.g. WaterOil, except
+# for LET parameters, where the API is simplified to 'l', 'e' and 't'.
+WO_ENDPOINTS = ["swirr", "swl", "swcr", "sorw"]
+WO_COREY_WATER = ["nw"]
+WO_WATER_ENDPOINTS = ["krwmax", "krwend"]
+WO_COREY_OIL = ["now"]
+WO_LET_WATER = ["Lw", "Ew", "Tw"]  # Will translated to l, e and t in code below.
+WO_LET_OIL = ["Low", "Eow", "Tow"]
+WO_OIL_ENDPOINTS = ["kromax", "kroend"]
 
 
 class PyscalFactory(object):
-    def createWaterOil(self, params):
+    """Class for implementing the factory pattern for Pyscal objects
 
+    The factory functions herein can take multiple parameter sets,
+    determine what kind of parametrization to be used, and set up
+    the full objects based on these parameters, instead of
+    explicitly having to call the API for each task.
+
+    Example:
+
+        wo = WaterOil(sorw=0.05)
+        wo.add_corey_water(nw=3)
+        wo.add_corey_oil(now=2)
+        # is equivalent to:
+        wo = factory.create_water_oil(dict(sorw=0.05, nw=3, now=2))
+    """
+
+    @staticmethod
+    def create_water_oil(params):
+        """Create a WaterOil object from a dictionary of parameters.
+
+        Parametrization (Corey/LET) is inferred from presence
+        of certain parameters in the dictionary.
+
+        Don't rely on behaviour of you supply both Corey and LET at
+        the same time.
+
+        NB: the add_LET_* methods have the names 'l', 'e' and 't'
+        in their signatures, which is not precise enough in this
+        situations, so we require e.g. 'Lw' (which will be
+        translated to 'l')
+        """
         usedparams = set()
         # No requirements to the base objects, defaults are ok.
-        wo = WaterOil(**slicedict(params, wo_endpoints + ["h"]))
+        wateroil = WaterOil(**slicedict(params, WO_ENDPOINTS + ["h"]))
         usedparams = usedparams.union(
-            set(slicedict(params, wo_endpoints + ["h"]).keys())
+            set(slicedict(params, WO_ENDPOINTS + ["h"]).keys())
+        )
+        logging.info(
+            "Initialized WaterOil object from parameters %s", str(list(usedparams))
         )
 
         # Water curve
-        params_corey_water = slicedict(params, wo_corey_water + wo_water_endpoints)
-        if wo_corey_water[0] in params_corey_water:
-            wo.add_corey_water(**params_corey_water)
-            logging.info("Added Corey water to WaterOil object")
+        params_corey_water = slicedict(params, WO_COREY_WATER + WO_WATER_ENDPOINTS)
+        params_let_water = slicedict(params, WO_LET_WATER + WO_WATER_ENDPOINTS)
+        if set(WO_COREY_WATER).issubset(set(params_corey_water)):
+            wateroil.add_corey_water(**params_corey_water)
             usedparams = usedparams.union(set(params_corey_water.keys()))
-        elif len(slicedict(params, wo_let_water)) == 3:
-            wo.add_let_water(**slicedict(params, wo_let_water))
-            logging.info("Added LET water to WaterOil object")
+            logging.info(
+                "Added Corey water to WaterOil object from parameters %s",
+                str(params_corey_water.keys()),
+            )
+        elif set(WO_LET_WATER).issubset(set(params_let_water)):
+            params_let_water["l"] = params_let_water.pop("Lw")
+            params_let_water["e"] = params_let_water.pop("Ew")
+            params_let_water["t"] = params_let_water.pop("Tw")
+            wateroil.add_LET_water(**params_let_water)
+            usedparams = usedparams.union(set(params_let_water.keys()))
+            logging.info(
+                "Added LET water to WaterOil object from parameters %s",
+                str(params_let_water.keys()),
+            )
         else:
             logging.warning(
                 "Missing or ambiguous parameters for water curve in WaterOil object"
             )
 
         # Oil curve:
-        if len(slicedict(params, wo_corey_oil)) == 1:
-            wo.add_corey_water(**slicedict(params, wo_corey_oil))
-            logging.info("Added Corey water to WaterOil object")
-        elif len(slicedict(params, wo_let_oil)) == 3:
-            wo.add_let_water(**slicedict(params, wo_let_oil))
-            logging.info("Added LET water to WaterOil object")
+        params_corey_oil = slicedict(params, WO_COREY_OIL + WO_OIL_ENDPOINTS)
+        params_let_oil = slicedict(params, WO_LET_OIL + WO_OIL_ENDPOINTS)
+        if set(WO_COREY_OIL).issubset(set(params_corey_oil)):
+            wateroil.add_corey_water(**params_corey_oil)
+            logging.info(
+                "Added Corey water to WaterOil object from parameters %s",
+                str(params_corey_oil.keys()),
+            )
+        elif set(WO_LET_OIL).issubset(set(params_let_oil)):
+            params_let_oil["l"] = params_let_oil.pop("Low")
+            params_let_oil["e"] = params_let_oil.pop("Eow")
+            params_let_oil["t"] = params_let_oil.pop("Tow")
+            wateroil.add_LET_oil(**params_let_oil)
+            logging.info(
+                "Added LET water to WaterOil object from parameters %s",
+                str(params_let_oil.keys()),
+            )
         else:
             logging.warning(
                 "Missing or ambiguous parameters for oil curve in WaterOil object"
             )
 
-        return wo
+        return wateroil
 
-    def createGasOil(params):
-        go = GasOil()
-        return go
+    def create_gas_oil(self, params):
+        """foo"""
+        gasoil = GasOil()
+        return gasoil
 
-    def createWaterOilGas(params):
-        wog = WaterOilGas()
-        return wog
+    def create_water_oil_gas(self, params):
+        """foo"""
+        wateroilgas = WaterOilGas()
+        return wateroilgas
 
-    def createSCALrecommendation(params):
+    def create_scal_recommendation(self, params):
+        """foo"""
         # Allow YAML-object?
-        wog_low = WaterOilGas(params["low"])
-        wog_base = WaterOilGas(params["base"])
-        wog_high = WaterOilGas(params["high"])
+        wateroil_low = WaterOilGas(params["low"])
+        wateroil_base = WaterOilGas(params["base"])
+        wateroil_high = WaterOilGas(params["high"])
         # Should we search for 'h' in the incoming dict?
         h_dict = dict(h=0.02)
-        scal = SCALrecommendation(wog_low, wog_base, wog_high, **h_dict)
+        scal = SCALrecommendation(wateroil_low, wateroil_base, wateroil_high, **h_dict)
         return scal
 
-    def createSCALrecommendationList(yamlobject):
+    def create_scal_recommendation_list(self, yamlobject):
         """Reserved"""
 
-    def createWOGList():
+    def create_wog_list(self):
         """Reserved for future implementation"""
         pass
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,17 +4,61 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import logging
+
 from pyscal import WaterOil, PyscalFactory
 
 
 def test_factory_wateroil():
+    """Test that we can create curves from dictionaries of parameters"""
+
+    logging.getLogger().setLevel("INFO")
+
     factory = PyscalFactory()
-    wo = factory.createWaterOil(dict(swirr=0.01, swl=0.1, bogus="foobar"))
+    wo = factory.create_water_oil(dict(swirr=0.01, swl=0.1, bogus="foobar"))
     assert isinstance(wo, WaterOil)
     assert wo.swirr == 0.01
     assert wo.swl == 0.1
+    assert "krw" not in wo.table  # A zero column would be okay though.
 
-    wo = factory.createWaterOil(dict(nw=3, krwend=0.2, krwmax=0.2))
+    # (static functions)
+    wo = PyscalFactory.create_water_oil(dict(nw=3, krwend=0.2, krwmax=0.5))
     assert isinstance(wo, WaterOil)
     assert "krw" in wo.table
-    assert wo.table["krw"].max() == 0.2
+    assert "Corey" in wo.krwcomment
+    assert wo.table["krw"].max() == 0.2  # Because sorw==0 by default
+
+    wo = factory.create_water_oil(dict(nw=3, sorw=0.1, krwend=0.2, krwmax=0.5))
+    assert isinstance(wo, WaterOil)
+    assert "krw" in wo.table
+    assert "Corey" in wo.krwcomment
+    assert wo.table["krw"].max() == 0.5
+
+    # Ambiguous works, but we don't guarantee that this results
+    # in LET or Corey.
+    wo = factory.create_water_oil(dict(nw=3, Lw=2, Ew=2, Tw=2))
+    assert "krw" in wo.table
+    assert "Corey" in wo.krwcomment or "LET" in wo.krwcomment
+
+    wo = factory.create_water_oil(dict(Lw=2, Ew=2, Tw=2, krwend=1))
+    assert isinstance(wo, WaterOil)
+    assert "krw" in wo.table
+    assert wo.table["krw"].max() == 1.0
+    assert "LET" in wo.krwcomment
+
+    wo = factory.create_water_oil(
+        dict(Lw=2, Ew=2, Tw=2, Low=3, Eow=3, Tow=3, krwend=0.5)
+    )
+    assert isinstance(wo, WaterOil)
+    assert "krw" in wo.table
+    assert "krow" in wo.table
+    assert wo.table["krw"].max() == 0.5
+    assert wo.table["krow"].max() == 1
+    assert "LET" in wo.krwcomment
+    assert "LET" in wo.krowcomment
+
+    # Some missing parameters, will give warnings.
+    wo = factory.create_water_oil(dict(Lw=2, Tw=2, Low=3, Eow=3))
+    assert isinstance(wo, WaterOil)
+    assert "krw" not in wo.table
+    assert "krow" not in wo.table
