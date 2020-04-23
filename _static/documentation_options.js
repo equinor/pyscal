e0784bf0a57f05f28643aa35641b19c5fb8d9133
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-23 06:28:47 +0000

    Deploy equinor/pyscal to github.com/equinor/pyscal.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.2.dev5+g4603595',
+    VERSION: '0.4.2.dev6+gb7b7821',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
