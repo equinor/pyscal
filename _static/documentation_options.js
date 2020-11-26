2becc8f86fa4a9ae2cc5b9e4aa96d95d6d2742fe
pyscal-github-action <pyscal-github-action>
2020-11-26 16:32:12 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.1.dev6+g29f8751',
+    VERSION: '0.7.1.dev7+gb46f720',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
