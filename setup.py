e391f4f1202e9df437613e03e5a56f355851d307
Håvard Berland <havb@equinor.com>
2021-09-01 10:55:54 +0200

    add packaging

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ REQUIREMENTS = [
     "numpy",
     "openpyxl",
     "pandas",
+    "packaging",
     "scipy",
     "typing-extensions>=3.7; python_version<'3.8'",
     "xlrd",
@@ -25,25 +26,33 @@ REQUIREMENTS = [
 
 TEST_REQUIREMENTS = Path("test_requirements.txt").read_text().splitlines()
 
-SETUP_REQUIREMENTS = ["pytest-runner", "setuptools>=42.0.0", "setuptools_scm"]
+SETUP_REQUIREMENTS = ["pytest-runner", "setuptools >= 28", "setuptools_scm"]
 EXTRAS_REQUIRE = {"tests": TEST_REQUIREMENTS}
 
 setup(
     name="pyscal",
-    use_scm_version={"write_to": "pyscal/version.py"},
-    cmdclass=cmdclass,
     description=(
         "Generate relative permeability include files for "
         "Eclipse reservoir simulator"
     ),
+    author="Håvard Berland",
+    author_email="havb@equinor.com",
+    url="http://github.com/equinor/pyscal",
+    use_scm_version={"write_to": "pyscal/version.py"},
+    project_urls=None,
+    keywords="relative permeability, capillary pressure, reservoir simulation",
+    license="LGPLv3",
+    platforms="any",
+    cmdclass=cmdclass,
+    include_package_data=True,
+    packages=find_packages(),
+    package_dir=None,
+    package_data={"pyscal": ["py.typed"]},
+    py_modules=None,
+    install_requires=REQUIREMENTS,
     entry_points={"console_scripts": ["pyscal = pyscal.pyscalcli:main"]},
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="http://github.com/equinor/pyscal",
-    author="Håvard Berland",
-    author_email="havb@equinor.com",
-    license="LGPLv3",
-    keywords="relative permeability, capillary pressure, reservoir simulation",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -57,11 +66,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=find_packages(),
-    package_data={"pyscal": ["py.typed"]},
     zip_safe=False,
     test_suite="tests",
-    install_requires=REQUIREMENTS,
     setup_requires=SETUP_REQUIREMENTS,
     extras_require=EXTRAS_REQUIRE,
 )
