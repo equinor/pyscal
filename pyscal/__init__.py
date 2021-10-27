f7def5b9dec3c33ecdadbe893d608cf429855a02
Tomáš Buček <tomas.bucek@student.tuke.sk>
2021-10-27 20:04:16 +0200

    fix docstring indent

--- pyscal/__init__.py
+++ pyscal/__init__.py
@@ -20,10 +20,9 @@ def getLogger_pyscal(
     Logging output is by default split by logging levels (split between WARNING and
     ERROR) to stdout and stderr, each log occurs in only one of the streams.
     Args:
-        module_name: A suggested name for the logger, usually
-            __name__ should be supplied
+        module_name: A suggested name for the logger, usually __name__ should be supplied
         args_dict: Dictionary with contents from the argparse namespace object containing
-            only keys "output", "verbose" and "debug".
+        only keys "output", "verbose" and "debug".
     """
     logger = logging.getLogger(module_name)
     if len(logger.handlers) != 0:
